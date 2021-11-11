413581a98083c847c62afb96d121b22a88de0e82
Daniel Akoto <sult4nbiz@gmail.com>
2021-11-11 08:02:06 -0500

    first ci

---
+++ public/app.js
@@ -0,0 +1,22 @@
+// const makeVid = document.getElementById('makeVid');
+const clear = document.getElementById('clear');
+const thingsList = document.getElementById('thingsList');
+
+var form = document.getElementById("form");
+form.addEventListener('submit', makeVid);
+
+function makeVid(event) {
+    event.preventDefault();
+    const input = document.getElementById('link')
+    let l = document.getElementById('link').value;
+    let link = l.substring(16, 29);
+    console.log(link);
+    thingsList.innerHTML = `<center>
+                        <iframe width="300px" height=200px"" src="https://www.youtube.com/embed/${link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </center>`
+    input.value = ''
+}
+                  
+clear.onclick = () => {
+    thingsList.innerHTML = ``;
+}
